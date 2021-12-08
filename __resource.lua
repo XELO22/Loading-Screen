f5004cf01871701471b1219939a5b9808ec258e9
XELO <82876564+XELO22@users.noreply.github.com>
2021-12-08 21:01:21 +0100

    .

---
+++ __resource.lua
@@ -0,0 +1,17 @@
+description 'XELO LOADING'
+
+loadscreen_manual_shutdown "yes"
+
+files {
+    'animate.css',
+    'app.js',
+    'index.css',
+    'index.html',
+    'index.js',
+    'music.mp3',
+    'particles.js',
+    '*.jpg',
+
+}
+
+loadscreen 'index.html'
